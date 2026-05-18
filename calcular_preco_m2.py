c766dc10c4cbdd5f326c2968ec0e2173d0984bb1
Lucas Matheus <lukaschamposki@gmail.com>
2026-05-18 15:31:27 -0300

    Refactor calcular_preco_m2 to operate on DataFrame in memory

--- calcular_preco_m2.py
+++ calcular_preco_m2.py
@@ -1,16 +1,17 @@
-
 import pandas as pd
-def calcular_preco_m2():
+
+
+def calcular_preco_m2(df: pd.DataFrame) -> pd.DataFrame:
     pd.options.display.float_format = '{:,.2f}'.format
 
-    df = pd.read_csv('dados.csv', sep= ';')
-
+    df = df.copy()
     df['preco_num'] = (
         df['preco']
+            .astype(str)
             .str.replace('R$', '', regex=False)
             .str.replace('.', '', regex=False)
             .str.replace(',', '.', regex=False)
             .astype(float)
     )
     df['preco_m2'] = df['preco_num'] / df['m2']
-    df.to_csv('dados.csv', sep=';', encoding='utf-8', index=False)
+    return df
